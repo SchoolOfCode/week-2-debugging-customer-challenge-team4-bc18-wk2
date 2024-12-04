1a3b9e97e414b546f5a95602936ca10875aab507
Conner Adamsons <185359332+ConnerConner96@users.noreply.github.com>
2024-12-04 15:41:41 +0000

    found and listed 10 bugs onto github, solved all bugs to allow game to run correctly.

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let score;
+let score = 0;
 let playAgain = true;
 
-while (playAgain === false) 
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  const userGuess = prompt("Guess a number between 1 and 10:");
+while (playAgain === true){
+  let randomNumber = Math.floor(Math.random() * 10) + 1;
+  let userGuess = prompt("Guess a number between 1 and 10:");
 
-  if (userGuess = randomnumber) {
+  if (userGuess == randomNumber){
     alert("Congratulations! You guessed the correct number.");
     score++;
   } else {
-    alert(`Sorry, the correct number was ${randomnumber}. You lose.`);
-    score++;
+    alert(`Sorry, the correct number was ${randomNumber}. You lose.`);
+  
   }
 
-  playAgain = confirm("Do you want to play again?");
+ playAgain = confirm("Do you want to play again?");
 
-  if (!playAgain); {
-    alert(`Game over. Your final score is ${randomNumber}.`);
+  if (!playAgain) {
+    alert(`Game over. Your final score is ${score}.`);
   }
 }
